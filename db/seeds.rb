270e5e09c45af035d8389d38dee001c8b0b169aa
Ahmed Atef <ahmedatef1610@gmail.com>
2022-01-25 15:16:55 +0200

    add User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "ahmed", password: "password")
+User.create(username: "atef", password: "password")
+User.create(username: "saad", password: "password")
+User.create(username: "omar", password: "password")
+
+
