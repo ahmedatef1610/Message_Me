62e993cd79b611f6f73188e968619e53b7240fc5
Ahmed Atef <ahmedatef1610@gmail.com>
2022-01-25 23:05:41 +0200

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_cable.mount_path = "/cable"
+  config.action_cable.url = "message-me-eg.herokuapp.com"
+  config.action_cable.allowed_request_origins = [ "https://message-me-eg.herokuapp.com/", /http:\/\/message-me-eg.herokuapp.com.*/ ]
 end
